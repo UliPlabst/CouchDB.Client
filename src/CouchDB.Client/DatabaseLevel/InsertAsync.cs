4413fb63f020be0334f86d51da72cbad59e194be
uli <up@plabst.org>
2019-04-05 15:08:44 +0200

    fix

--- src/CouchDB.Client/DatabaseLevel/InsertAsync.cs
+++ src/CouchDB.Client/DatabaseLevel/InsertAsync.cs
@@ -35,19 +35,6 @@ namespace CouchDB.Client
             return await client.http.ExecuteAsync(request);
         }
 
-        /// <summary>
-        /// http://docs.couchdb.org/en/2.2.0/api/database/common.html#specifying-the-document-id
-        /// Specify ID for insert
-        /// </summary>
-        /// <param name="json"></param>
-        /// <param name="batchMode">http://docs.couchdb.org/en/2.2.0/api/database/common.html#batch-mode-writes</param>
-        /// <returns></returns>
-        public async Task<CouchResponse> InsertAsync(JToken json, string id, bool batchMode = false)
-        {
-            json["_id"] = id;
-            return await this.InsertAsync(json, batchMode);
-        }
-
         public async Task<CouchResponse> InsertDesignDocumentAsync(string name, object json)
         {
             var request = new RestSharp.RestRequest(RestSharp.Method.PUT);
@@ -57,19 +44,6 @@ namespace CouchDB.Client
             if (res.ErrorException != null)
                 throw res.ErrorException;
             return res;
-        }
-
-        /// <summary>
-        /// http://docs.couchdb.org/en/2.1.2/api/database/common.html#post--db
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="obj"></param>
-        /// <param name="batchMode">http://docs.couchdb.org/en/2.1.2/api/database/common.html#batch-mode-writes</param>
-        /// <returns></returns>
-        public async Task<CouchResponse> InsertAsync<T>(T obj, bool batchMode = false)
-        {
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
-            return await this.InsertAsync(JToken.Parse(json), batchMode);
         }
     }
 }
